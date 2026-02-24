8112ca60722587cbbd90b929723b5d70ca9c0021
LevBaldin <levbaldin7@gmail.com>
2026-02-24 16:54:15 +0200

    prisma added

--- backend/index.ts
+++ backend/index.ts
@@ -1,1 +1,17 @@
-const express = require("express")
+import express, { application, request, response } from "express"
+import cors from "cors"
+import { createServer, Server as HTTPServer } from "http"
+import { Server } from "socket.io"
+const app: application = express()
+app.use(cors())
+const server: HTTPServer = createServer(app)
+const io: Server = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"]
+    }
+})
+
+server.listen(5000, () => {
+    console.log("Server is working.")
+})
