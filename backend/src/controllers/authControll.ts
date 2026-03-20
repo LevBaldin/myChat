7a6d6b88be3d2f70ce5a16989b18e6a8ec060151
LevBaldin <levbaldin7@gmail.com>
2026-03-20 14:46:42 +0200

    feat: add controller 'authConroll' to refresh tokens

---
+++ backend/src/controllers/authControll.ts
@@ -0,0 +1,34 @@
+import type { Request, Response } from "express"
+import { findUserByTokenRefresh } from "../services/findUserByTokenRefresh"
+import { generateJWT } from "../services/generateJWT"
+import { updateRefreshToken } from "../services/updateRefreshToken"
+export const refresh = async (req: Request, res: Response) => {
+    try {
+        const refreshToken = req.cookies.refreshToken
+        if (!refreshToken) return res.status(401).json({ message: "No refresh token" })
+        const userData = await findUserByTokenRefresh(refreshToken)
+        if (!userData) return res.status(403).json({ message: "Invalid refresh token" })
+        const { tokenAccess, tokenRefresh } = generateJWT(userData.user.id, userData.user.email)
+        await updateRefreshToken({ id: userData.user.id, tokenRefresh: tokenRefresh })
+
+        res.cookie("refreshToken", tokenRefresh, {
+            httpOnly: true,
+            secure: true,
+            sameSite: "lax",
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        })
+        res.cookie("accessToken", tokenAccess, {
+            httpOnly: true,
+            secure: true,
+            sameSite: "lax",
+            maxAge: 15 * 60 * 1000
+        })
+        res.status(200).json({
+            message: "Tokens refreshed"
+        })
+    } catch (error: unknown) {
+        if (error instanceof Error) {
+            res.status(500).json("Something wrong with server")
+        }
+    }
+}
