44430142eb032b42c066caac2d8c72b6298f23c9
LevBaldin <levbaldin7@gmail.com>
2026-04-24 15:53:03 +0300

    feat: add clear input button

--- frontend/src/components/ui/Input.tsx
+++ frontend/src/components/ui/Input.tsx
@@ -1,6 +1,8 @@
 "use client"
 
 import { SetState } from "@/app/auth/login/page"
+import Image from "next/image"
+import Cross from "../../../public/cross.svg"
 export interface InputProps<T> {
     type: string
     id: string
@@ -17,6 +19,12 @@ function Input<T>({ type, id, placeholder, label, setValue, value, required = tr
             [e.target.id]: e.target.value
         }))
     }
+    function clearInput() {
+        setValue((prev) => ({
+            ...prev,
+            [id]: ""
+        }))
+    }
     return (
         <div className={` text-xl ${label ? "justify-between " : "justify-center "} flex flex-col shrink gap-1 p-2 items-center `}>
             {label && (
@@ -24,16 +32,28 @@ function Input<T>({ type, id, placeholder, label, setValue, value, required = tr
                     {label}
                 </label>
             )}
-
-            <input
-                required={required}
-                onChange={handleChange}
-                type={type}
-                id={id}
-                value={value}
-                placeholder={placeholder}
-                className="justify-self-center bg-font/10 hover:bg-accent/8 p-2 rounded-md outline-font-active focus:outline-3 invalid:outline-orange-800 w-full text-md transition duration-100 shrink"
-            />
+            <div className="flex justify-between justify-self-center bg-font/10 hover:bg-accent/8 pr-2 rounded-md outline-font-active focus-within:outline-3 has-invalid:outline-orange-800 w-full text-md transition duration-100">
+                <input
+                    required={required}
+                    onChange={handleChange}
+                    type={type}
+                    id={id}
+                    value={value}
+                    placeholder={placeholder}
+                    className="p-2 rounded-md outline-none w-full transition caret-font"
+                />
+                {value && (
+                    <button type="button" onClick={clearInput}>
+                        <Image
+                            className="hover:opacity-80 hover:scale-110 transition cursor-pointer"
+                            src={Cross}
+                            width={20}
+                            height={20}
+                            alt="Clear input button"
+                        />
+                    </button>
+                )}
+            </div>
         </div>
     )
 }
