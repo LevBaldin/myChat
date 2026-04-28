f9aa53ca930aa07030d543beabbbce6b68f21e48
LevBaldin <levbaldin7@gmail.com>
2026-04-28 19:40:39 +0300

    feat: add selection panel to home page

--- frontend/src/app/home/page.tsx
+++ frontend/src/app/home/page.tsx
@@ -1,25 +1,15 @@
 "use client"
-import { logOut, setCredentials } from "@/store/authSlice"
-import Link from "next/link"
+import { logOut } from "@/store/authSlice"
 import { useRouter } from "next/navigation"
 import toast from "react-hot-toast"
 import { useDispatch } from "react-redux"
 import axiosInstance from "../lib/axiosInterseptor"
-import { useEffect } from "react"
+import Button from "@/components/ui/Button"
+import SelectionPanel from "@/components/SelectionPanel"
 
 export default function Page() {
     const dispatch = useDispatch()
     const router = useRouter()
-
-    async function getData() {
-        try {
-            const user = await axiosInstance.get("/userdata")
-            dispatch(setCredentials(user.data))
-            console.log(user)
-        } catch (error) {
-            console.log(error)
-        }
-    }
     async function handleLogOut() {
         try {
             const result = await toast.promise(axiosInstance.post("auth/logout"), {
@@ -30,18 +20,24 @@ export default function Page() {
             router.push("/auth/login")
             dispatch(logOut())
             return result
-        } catch (error) {
-            console.log(error)
+        } catch (error: unknown) {
+            // const errorMessage = error.response?.data?.message || "Error occured"
+            // toast.error(errorMessage)
+            console.dir(error)
         }
     }
-    useEffect(() => {
-        getData()
-    }, [])
+
     return (
-        <div>
-            <h2>Here will be main chat</h2>
-            <Link href={"auth/login"}> go back to login</Link>
-            <button onClick={handleLogOut}> log out</button>
-        </div>
+        <main className="flex h-dvh overflow-clip">
+            <div className="right-3 bottom-3 z-10 fixed">
+                <Button bg="main" onClick={handleLogOut}>
+                    log out
+                </Button>
+            </div>
+            <div className="hidden sm:block bg-primary w-full max-w-20 h-full" id="pref"></div>
+
+            <SelectionPanel />
+            <div className="w-full h-full" id="messages"></div>
+        </main>
     )
 }
